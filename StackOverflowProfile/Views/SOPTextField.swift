9d1822f185315971cda3762c160016356076de05
Md. Asadul Islam <asadul_islam@outlook.com>
2024-02-04 20:45:05 +0600

    Create custom textfield
    Signed-off-by: Md. Asadul Islam <asadul_islam@outlook.com>

---
+++ StackOverflowProfile/Views/SOPTextField.swift
@@ -0,0 +1,47 @@
+//
+//  SOPTextField.swift
+//  StackOverflowProfile
+//
+//  Created by Md. Asadul Islam on 4/2/24.
+//
+
+import UIKit
+
+class SOPTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        configure()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+
+        placeholder = "Enter user id"
+
+        backgroundColor = .tertiarySystemBackground
+
+        autocorrectionType = .no
+    }
+}
+
+#Preview {
+    SOPTextField(frame: .zero)
+}
